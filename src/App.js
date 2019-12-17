09d3170387e05f7973cb028f2b4a1d9f49fd9064
Erika <erika.maust@gmail.com>
2019-12-17 10:37:58 -0800

    React routes for library, search, and customer created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function Library() {
+  return <h2>Library</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+          <nav>
+            <ul>
+              <li>
+                <Link to="library">Library</Link>
+              </li>
+              <li>
+                <Link to="search">Search</Link>
+              </li>
+              <li>
+                <Link to="customers">Customers</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+    <Switch>
+      <Route path="/library">
+        <Library />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/customers">
+        <Customers />
+      </Route>
+    </Switch>
+    </Router>
     );
   }
+
+
 }
 
 export default App;
